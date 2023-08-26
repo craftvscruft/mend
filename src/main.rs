744647afc53dc2df9451a1eb7aaafb30e33aecc7
Ray Myers <ray.myers@gmail.com>
2023-08-25 19:04:18 -0500

    R** Propagate error results from load_mend

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use clap::{Parser, Error};
+use anyhow::{Context, anyhow};
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use std::fmt::Debug;
 use std::fs;
 use std::path::Path;
 use std::process::exit;
@@ -60,23 +62,30 @@ struct HookRunForTag {
 
 fn main() {
     run(&Cli::parse());
-    std::process::exit(1);
 }
 
 fn run(cli: &Cli) {
-    if let Ok(merged_mend) = load_mend(cli) {
-        if let Ok(text) = toml::to_string_pretty(&merged_mend) {
-            println!("{}", text);
-        } else {
+    match load_mend(cli) {
+        Ok(merged_mend) => {
+            match toml::to_string_pretty(&merged_mend) {
+                Ok(text) => {
+                    println!("{}", text);
+                }
+                Err(e) => {
+                    eprintln!("{e}");
+                    exit(1);
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("{e}");
             exit(1);
         }
-    } else {
-        exit(1);
     }
 
 }
 
-fn load_mend(cli: &Cli) -> Result<Mend, Error> {
+fn load_mend(cli: &Cli) ->anyhow::Result<Mend> {
     let parent_dir = Path::new(&cli.file)
         .parent()
         .expect("Unable to get the parent directory");
@@ -90,13 +99,9 @@ fn load_mend(cli: &Cli) -> Result<Mend, Error> {
         }
     };
 
-    let main_mend: Mend = match toml::from_str(&contents) {
-        Ok(d) => d,
-        Err(e) => {
-            eprintln!("Unable to load data from `{}` {}", &cli.file, e);
-            panic!()
-        }
-    };
+    let main_mend: Mend = toml::from_str(&contents)
+    .with_context(|| format!("Unable to load data from `{}`", &cli.file))?;
+
     let mut merged_mend: Mend = Mend {
         from: None,
         include: Vec::new(),
@@ -106,26 +111,12 @@ fn load_mend(cli: &Cli) -> Result<Mend, Error> {
         steps: Vec::new(),
     };
     for include_file in &main_mend.include {
-        let include_contents = match fs::read_to_string(parent_dir.join(include_file)) {
-            Ok(c) => c,
-            Err(e) => {
-                eprintln!("Could not read include file `{}` {}", &include_file, e);
-                panic!();
-            }
-        };
-        let include_mend: Mend = match toml::from_str(&include_contents) {
-            Ok(d) => d,
-            Err(e) => {
-                eprintln!("Unable to load data from `{}` {}", &include_file, e);
-                panic!();
-            }
-        };
+        let include_contents = fs::read_to_string(parent_dir.join(include_file))
+            .with_context(|| format!("Could not read include file `{}`", &include_file))?;
+        let include_mend: Mend = toml::from_str(&include_contents)
+            .with_context(|| format!("Unable to load data from `{}`", &include_file))?;
         if !include_mend.steps.is_empty() {
-            eprintln!(
-                "We only allow includes 1 level deep, sorry. Please restructure `{}`",
-                &include_file
-            );
-            panic!();
+            return Err(anyhow!("We only allow includes 1 level deep, sorry. Please restructure `{}`", &include_file))
         }
         extend_mend(&mut merged_mend, include_mend);
     }
